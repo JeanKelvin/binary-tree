4223c8feaf8bf5a72676ce864e58855ef8f01480
Jean Kelvin <jean_kelvin_05@hotmail.com>
2022-05-13 12:01:17 -0300

    binary tree

---
+++ src/tree/datastructure/BinaryTree.java
@@ -0,0 +1,19 @@
+package tree.datastructure;
+
+public class BinaryTree {
+    public int value;
+    public BinaryTree left;
+    public BinaryTree right;
+
+    public BinaryTree() {}
+
+    public BinaryTree(int value) {
+        this.value = value;
+    }
+
+    public BinaryTree(int value, BinaryTree left, BinaryTree right) {
+        this(value);
+        this.left = left;
+        this.right = right;
+    }
+}
